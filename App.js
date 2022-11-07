4a875ab0b587a4ea79ae7ff82305d397d714deb6
Max Willian <87044552+MaxWillianm@users.noreply.github.com>
2022-11-06 22:35:17 -0300

    Contrução da logica

--- App.js
+++ App.js
@@ -1,11 +1,78 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Keyboard } from 'react-native';
+import * as React from 'react';
+import { Provider as PaperProvider, Button, TextInput } from "react-native-paper";
+
+
+const CalcIMC = ({ label }) => {
+
+
+  const Resultado = async () => {
+    setResult(Peso / (Altura * Altura));
+    getIMC();
+
+  };
+
+  const [Peso, setPeso] = React.useState("");
+  const [Altura, setAltura] = React.useState("");
+  const [Result, setResult] = React.useState(0);
+  const [IMC, setIMC] = React.useState("");
+
+  const getIMC = async () => {
+    if (Result < 16) {
+      setIMC("Magreza grave");
+    }
+    if (Result > 16 && Result < 17) {
+      setIMC("Magreza moderada");
+    }
+  };
+
+
+
+  return (
+
+    <View style={styles.container}>
+      <Text>Informa abaixo o seu peso e sua altura</Text>
+      <TextInput
+        keyboardType='Numeric'
+        label="informe o seu peso"
+        value={Peso}
+        onChangeText={text => setPeso(text)}
+        style={styles.input}
+      />
+
+      <TextInput
+        keyboardType='Numeric'
+        label="Informe a sua altura"
+        value={Altura}
+        onChangeText={text => setAltura(text)}
+        style={styles.input}
+      />
+      <Button
+        style={styles.botao}
+        mode="elevated"
+        onPress={() => {
+          Resultado();
+          Keyboard.dismiss();
+        }}
+      >
+        calcular
+      </Button>
+      <Text>O seu IMC é: {parseFloat(Result).toFixed(3)}</Text>
+      <Text> voce tem: {IMC}</Text>
+
+
+    </View>
+  );
+};
+
+
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <CalcIMC />
     </View>
   );
 }
@@ -17,4 +84,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    height: 50,
+    width: 300,
+    margin: 10,
+    backgroundColor: "#D9DEDE",
+  },
+  botao: {
+    backgroundColor: "#FCFF00",
+    height: 40,
+    width: 200,
+  }
 });
